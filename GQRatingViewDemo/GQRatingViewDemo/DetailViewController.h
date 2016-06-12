014b264d66737150cdb382dfa6ab52a6c4425f02
developer_高 <763007297@qq.com>
2016-06-12 11:43:33 +0800

    add code

---
+++ GQRatingViewDemo/GQRatingViewDemo/DetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.h
+//  GQRatingViewDemo
+//
+//  Created by 高旗 on 16/6/12.
+//  Copyright © 2016年 gaoqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
+
